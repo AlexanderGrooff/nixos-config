738b190515dedef28a609b39bc202d671eb7fd3c
Alexander Grooff <alexandergrooff@gmail.com>
2024-04-12 23:08:55 +0200

    feat: sync local

--- sync.sh
+++ sync.sh
@@ -3,11 +3,19 @@
 set -e
 set -x
 
-TARGET="10.0.0.26"
-HOSTNAME="mu"
+# Get TARGET and HOSTNAME from the environment
+HOSTNAME=${HOSTNAME:-$(hostname)}
 
 nix run github:kamadorueda/alejandra/3.0.0 -- .
 git add .
 nix flake check
-rsync -rd --rsync-path="sudo rsync" --chown=root:root "${PWD}/" $TARGET:/etc/nixos
-ssh $TARGET sudo nixos-rebuild switch --flake "/etc/nixos/#$HOSTNAME"
+
+if [ -z "$TARGET" ]; then
+    sudo rsync -rd "${PWD}/" /etc/nixos
+    sudo nixos-rebuild switch --flake "/etc/nixos/#$HOSTNAME"
+    bash /etc/nixos/cleanup.sh
+else
+    rsync -rd --rsync-path="sudo rsync" --chown=root:root "${PWD}/" $TARGET:/etc/nixos
+    ssh $TARGET sudo nixos-rebuild switch --flake "/etc/nixos/#$HOSTNAME"
+    ssh bash /etc/nixos/cleanup.sh
+fi
